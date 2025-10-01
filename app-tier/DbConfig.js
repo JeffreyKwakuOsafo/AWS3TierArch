f5ab7217bbf6b1402c33b48549090e725ef095c4
JeffreyKwakuOsafo <155450238+JeffreyKwakuOsafo@users.noreply.github.com>
2025-10-01 13:16:31 +0000

    app-tier/

--- app-tier/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST : 'database-1-instance-1.cmv8kwm2au7z.us-east-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : 'nallpy12!',
-    DB_DATABASE : 'webappdb'
-});
